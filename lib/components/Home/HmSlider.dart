95bd086b7f74b225d8390d476a53a8722dddc44f
jjttkx <2901897062@qq.com>
2026-01-03 22:56:27 +0800

    轮播图的搜索框和指示灯

--- lib/components/Home/HmSlider.dart
+++ lib/components/Home/HmSlider.dart
@@ -11,28 +11,86 @@ class HmSlider extends StatefulWidget {
 }
 
 class _HmSliderState extends State<HmSlider> {
+  CarouselSliderController _controller = CarouselSliderController();
+  int _currentIndex = 0;
   Widget _getSlider() {
-    //在flutter中获取屏幕宽度
-    final double screenWidth = MediaQuery.of(context).size.width;
     return CarouselSlider(
+      carouselController: _controller,
       items: List.generate(widget.bannerList.length, (int index) {
         return Image.network(
           widget.bannerList[index].imgUrl,
           fit: BoxFit.cover,
-          width: screenWidth,
+          width: double.infinity,
         );
       }),
       options: CarouselOptions(
         height: 200,
         viewportFraction: 1,
+        onPageChanged: (int index, reason) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
         autoPlay: true,
-        autoPlayInterval: Duration(milliseconds: 1000),
+        // autoPlayInterval: Duration(milliseconds: 1000),
+      ),
+    );
+  }
+
+  Widget _getSearch() {
+    return Positioned(
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Container(
+          alignment: Alignment.centerLeft,
+          padding: EdgeInsets.symmetric(horizontal: 40),
+          height: 50,
+          decoration: BoxDecoration(
+            color: const Color.fromRGBO(0, 0, 0, 0.2),
+            borderRadius: BorderRadius.circular(25),
+          ),
+          child: Text("搜索", style: TextStyle(color: Colors.white)),
+        ),
+      ),
+    );
+  }
+
+  Widget _getDots() {
+    return Positioned(
+      left: 0,
+      right: 0,
+      bottom: 10,
+      child: SizedBox(
+        height: 40,
+        width: double.infinity,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List.generate(widget.bannerList.length, (int index) {
+            return GestureDetector(
+              onTap: () {
+                _controller.jumpToPage(index);
+              },
+              child: AnimatedContainer(
+                duration: Duration(milliseconds: 300),
+                height: 6,
+                width: index == _currentIndex ? 40 : 12,
+                margin: EdgeInsets.symmetric(horizontal: 4),
+                decoration: BoxDecoration(
+                  color: index == _currentIndex
+                      ? Colors.white
+                      : Color.fromRGBO(0, 0, 0, 0.2),
+                  borderRadius: BorderRadius.circular(3),
+                ),
+              ),
+            );
+          }),
+        ),
       ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Stack(children: [_getSlider()]);
+    return Stack(children: [_getSlider(), _getSearch(), _getDots()]);
   }
 }
